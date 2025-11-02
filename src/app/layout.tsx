b6b66c4c58a40cdbf513121c00d8f2c9c0fb5e2e
Murtaza Saifuddin <mortuze@gmail.com>
2025-11-02 15:31:43 -0500

    chore(icon): icon set changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="apple-mobile-web-app-title" content="Box" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
