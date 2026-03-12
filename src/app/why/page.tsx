45fef26c5642aef007d5f6981ecebcc615671906
Murtaza Saifuddin <mortuze@gmail.com>
2026-03-12 00:24:50 -0400

    Refactor WhyPage content to use Markdown format
    - Consolidated the WHY_CONTENT and USES_CONTENT arrays into a single Markdown string for better readability and maintainability.
    - Updated the rendering of content in the WhyPage component to utilize the new Markdown format.

--- src/app/why/page.tsx
+++ src/app/why/page.tsx
@@ -1,17 +1,20 @@
 import TextContent from "@/components/content/TextContent";
 import React from "react";
 
-const WHY_CONTENT = [
-  `I see the people that build products and services and showcase them to the world and I always wanted to do that, I have built a lot for other people and my job but I dont think I am getting any better. So this project serves two things, one is to build something cool that I would use myself and the other is to get feedback from the community on my code my design and my ideas.`,
-  `This is my the github link to the project: https://github.com/saifuddm/box. I believe I have setup branch protection and hopefully not leaking any secrets, but if you do find some secret or some exploit on the site please please let me know. When I release new box features I will give you a preview as a thanks.`,
-];
+const WHY_PAGE_MARKDOWN = `
+# Why we built Box?
 
-const USES_CONTENT = [
-  `I create grocery lists and share with my family so they can add anything I missed, and because it gets deleted in 24 hours there is pressure for me to actually go and buy the grocery haha.`,
-  `I use it to pass information between my devices from Iphone to Windows without having to sign in to anything, why not google drive? IDK`,
-  `Used it one time to plan a party with friends. Food options, activities, locations.`,
-  `My friend one time used it to send a PDF document to the public print computers.`,
-];
+I see the people that build products and services and showcase them to the world and I always wanted to do that, I have built a lot for other people and my job but I dont think I am getting any better. So this project serves two things, one is to build something cool that I would use myself and the other is to get feedback from the community on my code my design and my ideas.
+
+This is my the github link to the project: [box](https://github.com/saifuddm/box). I believe I have setup branch protection and hopefully not leaking any secrets, but if you do find some secret or some exploit on the site please please let me know. When I release new box features I will give you a preview as a thanks.
+
+# What do we use Box for?
+
+- I create grocery lists and share with my family so they can add anything I missed, and because it gets deleted in 24 hours there is pressure for me to actually go and buy the grocery haha.
+- I use it to pass information between my devices from Iphone to Windows without having to sign in to anything, why not google drive? IDK
+- Used it one time to plan a party with friends. Food options, activities, locations.
+- My friend one time used it to send a PDF document to the public print computers.
+`;
 
 function WhyPage() {
   return (
@@ -21,29 +24,9 @@ function WhyPage() {
     >
       <TextContent
         id="why"
-        content="Why we built Box?"
+        content={WHY_PAGE_MARKDOWN}
         className="lg:col-span-3"
       />
-      {WHY_CONTENT.map((content, index) => (
-        <TextContent
-          key={`content-${index}`}
-          id={`content-${index}`}
-          content={content}
-        />
-      ))}
-
-      <TextContent
-        id="uses"
-        content="What do we use Box for?"
-        className="lg:col-span-3"
-      />
-      {USES_CONTENT.map((content, index) => (
-        <TextContent
-          key={`content-uses-${index}`}
-          id={`content-uses-${index}`}
-          content={content}
-        />
-      ))}
     </div>
   );
 }
